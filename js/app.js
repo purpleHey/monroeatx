8eff1f9a457653ac77d2f37c89dee1ae5d315c26
W. Wesley Monroe <ww.monroe@gmail.com>
2015-04-13 15:30:44 -0500

    Uploading to my monreatx repo

---
+++ js/app.js
@@ -0,0 +1,57 @@
+//when onready, run this code
+$(function() {
+
+	console.log('loaded');
+
+	var nav = $('#sidebar-wrapper .sidebar-nav');
+	var mainContentArea = $('#page-content-wrapper');	
+
+	var links = nav.find('a');
+
+	var currentActivePage = nav.find('.active').attr('href').replace('#', '');
+
+	function updateNavigation(selectedNavItem) {
+		links.removeClass('active');
+		selectedNavItem.addClass('active');
+	}
+
+	function onclick_link (e) {
+		e.preventDefault();
+		var element = $(this);
+		var href = element.attr('href').replace('#', '');
+		
+		console.log(href);
+		getPage(href);
+		updateNavigation(element);
+
+	}
+
+	function getPage(pageName) {
+
+		//call the server
+		if (!pageName) {return;}
+
+		var route = '/pages/';
+
+		$.ajax({
+			url: route + pageName + '.html',
+			type: 'GET',
+			dataType: 'html',
+			success: function (response) {
+				mainContentArea.html(response);
+			},
+			error: function (response) {
+				console.log('ERROR:', response);
+			}
+		});
+
+	}
+
+	$.each(links, function(i, link) {
+		link = $(link);
+		link.on('click', onclick_link);
+	});
+
+	getPage(currentActivePage);
+
+});
